7a893bc3691930d3919a76bd594928093420a212
juner <juner@juner.net>
2019-01-10 14:29:17 +0900

    コマンドプロンプトの終了のサポート

--- SshClientConsole/Program.cs
+++ SshClientConsole/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -12,85 +13,136 @@ namespace SshClientConsole {
     class Program {
         static void Main(string[] args)
         {
-            var TokenSource = new CancellationTokenSource();
-            var app = new CommandLineApplication(throwOnUnexpectedArg: false) {
-                Name = nameof(SshClientConsole),
-            };
-            app.HelpOption("-?|-h|--help");
-            var UserNameOption = app.Option("-user|--username <username>", "User Name", CommandOptionType.SingleValue);
-            var PasswordOption = app.Option("-p|--password <password>", "Password", CommandOptionType.SingleValue);
-            var HostNameOption = app.Option("-host|--hostname <hostname>", "Host Name", CommandOptionType.SingleValue);
-            app.OnExecute(async () => {
-                var Token = TokenSource.Token;
-                var UserName = UserNameOption.Value() ?? throw new ArgumentNullException(UserNameOption.ValueName);
-                var Password = PasswordOption.Value() ?? throw new ArgumentNullException(PasswordOption.ValueName);
-                var HostName = HostNameOption.Value() ?? throw new ArgumentNullException(HostNameOption.ValueName);
-                var end = new Regex(@"[$#>:][ ]$", RegexOptions.Multiline);
-                var ConsoleWait = TimeSpan.FromMilliseconds(100);
-                try {
-                    using (var writer = new TextWriterTraceListener(Console.Out)) {
-                        Trace.Listeners.Add(writer);
-                        var AuthMethod = new PasswordAuthenticationMethod(UserName, Password);
-                        var info = new ConnectionInfo(HostName, UserName, AuthMethod) {
-                            Encoding = System.Text.Encoding.UTF8
-                        };
-                        var Encoding = info.Encoding;
-                        using (var client = new SshClient(info)) {
-                            client.Connect();
-                            var buffer = new byte[1024].AsMemory();
-                            var stream = client.CreateShellStream(string.Empty, 0, 0, 0, 0, 1024);
+            using (var TokenSource = new CancellationTokenSource())
+            using (SetConsoleCtrlHandler(ctrl => {
+                TokenSource.Cancel();
+                return false;
+            })) {
+                var app = new CommandLineApplication(throwOnUnexpectedArg: false) {
+                    Name = nameof(SshClientConsole),
+                    FullName = "SSH.NET SshClient -> ShellStream Example",
+                };
+                app.HelpOption("-?|-h|--help");
+                var UserNameOption = app.Option("-user|--username <username>", "User Name", CommandOptionType.SingleValue);
+                var PasswordOption = app.Option("-p|--password <password>", "Password", CommandOptionType.SingleValue);
+                var HostNameOption = app.Option("-host|--hostname <hostname>", "Host Name", CommandOptionType.SingleValue);
+                app.OnExecute(async () => {
+                    var Token = TokenSource.Token;
+                    var UserName = UserNameOption.Value() ?? throw new ArgumentNullException(UserNameOption.ValueName);
+                    var Password = PasswordOption.Value() ?? throw new ArgumentNullException(PasswordOption.ValueName);
+                    var HostName = HostNameOption.Value() ?? throw new ArgumentNullException(HostNameOption.ValueName);
+                    var end = new Regex(@"[$#>:][ ]$", RegexOptions.Multiline);
+                    var ConsoleWait = TimeSpan.FromMilliseconds(100);
+                    try {
+                        using (var writer = new TextWriterTraceListener(Console.Out)) {
+                            Trace.Listeners.Add(writer);
+                            var AuthMethod = new PasswordAuthenticationMethod(UserName, Password);
+                            var info = new ConnectionInfo(HostName, UserName, AuthMethod) {
+                                Encoding = System.Text.Encoding.UTF8
+                            };
+                            var Encoding = info.Encoding;
+                            using (var client = new SshClient(info)) {
+                                client.Connect();
+                                var buffer = new byte[1024].AsMemory();
+                                var stream = client.CreateShellStream(string.Empty, 0, 0, 0, 0, 1024);
 
-                            var ExpectTask = stream.ExpectOnceAsync(end);
-                            do {
-                                if (stream.DataAvailable) {
-                                    var streamLength = stream.Length;
-                                    var length = stream.Length > int.MaxValue || (int)streamLength > buffer.Length ? buffer.Length : (int)streamLength;
-                                    var mem = buffer.Slice(0, length);
-                                    length = await stream.ReadAsync(mem, Token);
-                                    Trace.Write(Encoding.GetString(mem.Slice(0, length).Span));
-                                } else {
-                                    await Task.Delay(ConsoleWait);
-                                }
-                                if (ExpectTask.IsCompleted && !stream.DataAvailable)
-                                    break;
-                            } while (true);
-                            Trace.Write(await ExpectTask);
-                            var line = string.Empty;
-                            do {
-                                try {
+                                var ExpectTask = stream.ExpectOnceAsync(end, Token);
+                                do {
+                                    if (stream.DataAvailable)
+                                        Trace.Write(await ReadAsync(stream, buffer, Encoding, Token));
+                                    else
+                                        await Task.Delay(ConsoleWait);
+                                    if (ExpectTask.IsCompleted && !stream.DataAvailable)
+                                        break;
+                                } while (!Token.IsCancellationRequested);
+                                Trace.Write(await ExpectTask);
+                                var line = string.Empty;
+                                do {
+                                    try {
+                                        line = Console.ReadLine();
+                                        stream.WriteLine(line);
+                                        ExpectTask = stream.ExpectOnceAsync(end, Token);
+                                        do {
+                                            if (stream.DataAvailable)
+                                                Trace.Write(await ReadAsync(stream, buffer, Encoding, Token));
+                                            else
+                                                await Task.Delay(ConsoleWait);
 
-                                    line = Console.ReadLine();
-                                    stream.WriteLine(line);
-                                    ExpectTask = stream.ExpectOnceAsync(end);
-                                    do {
-                                        if (stream.DataAvailable) {
-                                            var streamLength = stream.Length;
-                                            var length = stream.Length > int.MaxValue || (int)streamLength > buffer.Length ? buffer.Length : (int)streamLength;
-                                            var mem = buffer.Slice(0, length);
-                                            length = await stream.ReadAsync(mem, Token);
-                                            Trace.Write(Encoding.GetString(mem.Slice(0, length).Span));
-                                        } else {
-                                            await Task.Delay(ConsoleWait);
-                                        }
-                                        if (ExpectTask.IsCompleted && !stream.DataAvailable)
-                                            break;
-                                    } while (true);
-                                    Trace.Write(await ExpectTask);
-                                } catch (Exception e) {
-                                    Trace.WriteLine(e);
-                                }
-                            } while (line != string.Empty);
-                            if (client.IsConnected)
-                                client.Disconnect();
+                                            if (ExpectTask.IsCompleted && !stream.DataAvailable)
+                                                break;
+                                        } while (!Token.IsCancellationRequested);
+                                        Trace.Write(await ExpectTask);
+                                    } catch (Exception e) {
+                                        Trace.WriteLine(e);
+                                    }
+                                } while (line != string.Empty);
+                            }
                         }
+                        return 0;
+                    } catch (TaskCanceledException e) {
+                        Trace.WriteLine("Canceled.");
+                        return e.HResult;
+                    } catch (Exception e) {
+                        Trace.WriteLine(e);
+                        return e.HResult;
                     }
-                    return 0;
-                } catch (Exception e) {
-                    Trace.WriteLine(e);
-                    return e.HResult;
+                });
+                app.Execute(args);
+            }
+        }
+        /// <summary>
+        /// <paramref name="stream"/>を末尾まで文字列として読み込む
+        /// </summary>
+        /// <param name="stream">読み込み元</param>
+        /// <param name="buffer">バッファ</param>
+        /// <param name="Encoding">エンコード</param>
+        /// <param name="Token">キャンセル用</param>
+        /// <returns></returns>
+        static async Task<string> ReadAsync(ShellStream stream, Memory<byte> buffer, Encoding Encoding, CancellationToken Token = default)
+        {
+            Token.ThrowIfCancellationRequested();
+            var streamLength = stream.Length;
+            var length = stream.Length > int.MaxValue || (int)streamLength > buffer.Length ? buffer.Length : (int)streamLength;
+            var mem = buffer.Slice(0, length);
+            length = await stream.ReadAsync(mem, Token);
+            if (length == 0)
+                return string.Empty;
+            return Encoding.GetString(mem.Slice(0, length).Span);
+        }
+        static IDisposable SetConsoleCtrlHandler(Func<CtrlTypes, bool> Action)
+        {
+            var HandlerRoutine = new NativeMethods.HandlerRoutine(Action);
+            var result = NativeMethods.SetConsoleCtrlHandler(HandlerRoutine, true);
+            if (!result)
+                return null;
+            return Disposable.Create(() => NativeMethods.SetConsoleCtrlHandler(HandlerRoutine, false));
+        }
+        static class NativeMethods {
+            [DllImport("Kernel32")]
+            public static extern bool SetConsoleCtrlHandler(HandlerRoutine Handler, bool Add);
+            public delegate bool HandlerRoutine(CtrlTypes CtrlType);
+        }
+        enum CtrlTypes {
+            CTRL_C_EVENT = 0,
+            CTRL_BREAK_EVENT = 1,
+            CTRL_CLOSE_EVENT = 2,
+            CTRL_LOGOFF_EVENT = 5,
+            CTRL_SHUTDOWN_EVENT = 6
+        }
+        public class Disposable : IDisposable {
+            readonly Action Action;
+            Disposable(Action Action) => this.Action = Action;
+            public static IDisposable Create(Action Action) => new Disposable(Action);
+            private bool disposedValue = false;
+            protected virtual void Dispose(bool disposing)
+            {
+                if (!disposedValue) {
+                    if (disposing)
+                        Action.Invoke();
+                    disposedValue = true;
                 }
-            });
-            app.Execute(args);
+            }
+            public void Dispose() => Dispose(true);
         }
     }
 }
